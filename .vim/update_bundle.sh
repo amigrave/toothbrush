0384e2d949b7380394ac469c6846b6af3e952e81
Fabien Meghazi <agr@amigrave.com>
2013-01-28 10:44:55 +0100

    update vim bundle list

--- .vim/update_bundle.sh
+++ .vim/update_bundle.sh
@@ -17,7 +17,7 @@ git clone https://github.com/kchmck/vim-coffee-script.git
 git clone https://github.com/vim-scripts/ASPJScript.git
 git clone https://github.com/plasticboy/vim-markdown.git
 git clone https://github.com/marzocchi/vim-color-picker-osx.git
-#git clone https://github.com/jelera/vim-javascript-syntax
-#git clone https://github.com/vim-scripts/JavaScript-syntax.git
+git clone https://github.com/jelera/vim-javascript-syntax
 git clone https://github.com/mileszs/ack.vim.git
-git clone https://github.com/amigrave/vim-pudb
+#git clone https://github.com/amigrave/vim-pudb
+git clone https://github.com/tpope/vim-commentary
