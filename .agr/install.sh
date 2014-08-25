6058f8b0aecf5dc7f00295e808be9b914e271372
Fabien Meghazi <agr@amigrave.com>
2014-08-25 23:52:27 +0200

    List python packages needed for dev

--- .agr/install.sh
+++ .agr/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 apt-get -y install mc screen rsync vim telnet strace less netcat
+
+pip install Baker bzr cdiff fabric fabtools flake8 git-qdiff httpie ipython jedi pudb pyquery pythonpy requests virtualenv pyrasite
+
+# osx
+pip install appscript
+brew install homebrew/dupes/gdb # for pyrasite
