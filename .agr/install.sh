83839ab137d9e74359c2baba18d1bd65fb500364
Fabien Meghazi <agr@amigrave.com>
2014-08-31 23:22:57 +0200

    Some TODOs for install.sh

--- .agr/install.sh
+++ .agr/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-apt-get -y install mc screen rsync vim telnet strace less netcat
+
+mkdir -p ~/.vim/swap ~/.vim/backup
+
+# linux
+# TODO: https://help.ubuntu.com/community/AppleKeyboard#Change%20Function%20Key%20behavior
+# TODO: http://askubuntu.com/questions/7537/how-can-i-reverse-the-fn-key-on-an-apple-keyboard-so-that-f1-f2-f3-are-us
+# TODO: http://askubuntu.com/questions/149971/how-do-you-remap-a-key-to-the-caps-lock-key-in-xubuntu
+apt-get -y install mc screen tmux htop rsync vim telnet strace less netcat ncurses-term openssh-server python-pip
 
 pip install Baker bzr cdiff fabric fabtools flake8 git-qdiff httpie ipython jedi pudb pyquery pythonpy requests virtualenv pyrasite
 
