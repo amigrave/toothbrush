8a75f9a7c66f9e261015b3378b20c64d48319556
Fabien Meghazi <agr@amigrave.com>
2014-12-11 15:10:36 +0100

    move orhpan scripts and automation to agr.py

--- .agr/install.sh
+++ .agr/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/swap ~/.vim/backup
-
 # linux
 # TODO: https://help.ubuntu.com/community/AppleKeyboard#Change%20Function%20Key%20behavior
 # TODO: http://askubuntu.com/questions/7537/how-can-i-reverse-the-fn-key-on-an-apple-keyboard-so-that-f1-f2-f3-are-us
@@ -11,9 +9,27 @@ apt-get -y install mc screen tmux htop rsync vim telnet strace less netcat ncurs
 pip install Baker bzr cdiff fabric fabtools flake8 git-qdiff httpie ipython jedi pudb pyquery pythonpy requests virtualenv pyrasite
 
 # osx
+# Some symlinks
+ln -s ~/.dosbox/dosbox-0.74.conf Library/Preferences/DOSBox\ 0.74\ Preferences
+
+# Change some defaults
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+
+brew install coreutils --default-names
+brew install findutils --default-names
+brew install htop
+chmod 6555 /usr/local/Cellar/htop/HEAD/bin/htop
+sudo chown root /usr/local/Cellar/htop/HEAD/bin/htop
+
+brew install ack autossh bazaar cd-discid colordiff flac fuse4x fuse4x-kext gdbm gettext glib htop id3lib id3v2 lame lha libao libiconv libid3tag libmikmod libogg libvorbis mad midnight-commander mikmod mpg321 mplayer node oniguruma p7zip pcre pkg-config pyqt qt renameutils s-lang sip smake sshfs tig uade unrar vorbis-tools wget xmp yasm zsh
+
 pip install appscript
 brew install homebrew/dupes/gdb # for pyrasite
 PYTHON=/usr/local/bin/python3 brew install --with-python postgresql
+
 
 
 # GIT: show author on rebase interactive:
--- bin/agr.py
+++ bin/agr.py
@@ -72,3 +72,272 @@ def ppast(code):
     """
     # print dump(ast.parse(code, filename=filename), include_attributes=True)
     print dump(ast.parse(code), include_attributes=True)
+
+#############################################################
+# Wakeup
+#
+# import socket
+# s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# #s.sendto('\xff'*6+'\x01\x23\x45\x67\x89\x0a'*16, ('192.168.1.255', 80))
+# s.sendto('\xff'*6+'\x00\x1F\x3B\x13\xC2\x43'*16, ('192.168.1.255', 80))
+
+#############################################################
+# rwed (set up chmod for amiga executable under uae
+#
+# #!/usr/bin/ruby
+# require "find"
+# Find.find(".") do |path|
+# 	if `file -b "#{path}"`.strip == "AmigaOS loadseg()ble executable/binary"
+# 		cmd = "chmod u+rwx,og-rwx \"#{path}\""
+# 		puts cmd
+# 		system(cmd)
+# 	end
+# end
+######## PLUS WHD RENAMES
+# #!/bin/bash
+# mv *.info WHD-Game.info
+# mv *.slave WHD-Game.slave
+# mv *.Slave WHD-Game.slave
+# agr_chmod
+# #chmod a-x *
+# mv readme ReadMe
+# mv solution Solution
+# mv manual Manual
+# mv hints Hints
+# mv instructions Manual
+# mv Instructions Manual
+#
+########### SAME FOR DEMOS
+# mv *.info WHD-Demo.info
+# mv *.slave WHD-Demo.slave
+# mv *.Slave WHD-Demo.slave
+# agr_chmod
+# #chmod a-x *
+# mv readme ReadMe
+
+#############################################################
+# apple keyboard
+#
+# #! /bin/bash
+# kbdrate -r 30 -d 10
+# xset r on
+
+#############################################################
+# image dates (TODO: image compression and resize)
+#
+# #!/usr/bin/python
+# import os, sys, commands, re
+# if len(sys.argv) == 1:
+# 	name = sys.argv[0].split("/").pop()
+# 	print "Usage: %s <image file(s)> " % (name)
+# 	sys.exit()
+
+# for file in sys.argv[1:]:
+# 	try:
+# 		date = commands.getoutput("exif %s | grep 'Date and Time'" % file).split("\n")[0].split("|")[1].strip().replace(":", "-", 2)
+# 		print "%s : Will set date to %s" % (file, date)
+# 		cmd = "touch -d '%s' \"%s\"" % (date, file)
+# 		commands.getoutput(cmd)
+# 	except:
+# 		print "%s : Can't find EXIF data inside" % file
+
+#############################################################
+# TODO: music related
+# FLAC2OGG.py
+# import os, sys, commands, re
+#
+# quality = 6
+# if len(sys.argv) == 3 and sys.argv[1] == "-q" and re.match("^[1-9]$", sys.argv[2]):
+# 	quality = sys.argv[2]
+#
+# oggenc = 'oggenc -q %s --artist "%s" --album "%s" --title "%s" --date "%s" --tracknum "%s" --genre "%s" "%s"'
+#
+# files = os.listdir("./")
+#
+# for i in files:
+# 	ext = i.split(".")[-1]
+# 	if ext == "flac":
+# 		print "Encoding " + i
+# 		metas = {'artist' : '', 'album' : '', 'title' : '', 'date' : '', 'tracknumber' : '', 'genre' : ''}
+# 		#print "metaflac --list \"" + i.replace('"', '\\"') + "\" | grep 'comment\['"
+# 		meta = commands.getoutput("metaflac --list \"" + i.replace('"', '\\"') + "\" | grep 'comment\['").splitlines()
+# 		for j in meta:
+# 			p = j.split("]: ")[1].split("=")
+# 			metas[p[0].lower()] = p[1]
+# 		for j in metas:
+# 			metas[j] = metas[j].replace('"', '\"')
+# 		cmd = oggenc % (quality, metas["artist"], metas["album"], metas["title"], metas["date"], metas["tracknumber"], metas["genre"], i)
+# 		#print cmd
+# 		status = commands.getoutput(cmd)
+#
+#############################################################
+# FLAC2OGG.rb
+#!/usr/bin/ruby
+# require "shellwords"
+# require "open3"
+#
+# def is_installed?(prog)
+# 	return `which #{prog}`.strip != ""
+# end
+#
+# def is_flac?(file)
+# 	# todo
+# 	# 66 4C 61 43 : fLaC
+# 	#hex = IO.popen("hexdump -c -n 10000 \"#{file}\"").readlines.join("").gsub(" ", "")
+# 	#p hex.count("fLaCd")
+# 	return false unless File.exists? file
+# 	(stdin, stdout, stderr) = Open3.popen3("metaflac --export-tags-to=- #{Shellwords.escape(file)}")
+# 	return stderr.read.strip.empty?
+# end
+#
+# "flac,metaflac,oggenc".split(",").each do |prog|
+# 	abort "#{prog} not found, please install this program first" unless is_installed? prog
+# end
+#
+# abort "Nothing to convert" if ARGV.empty?
+#
+# ARGV.each do |file|
+# 	flac_file = File.expand_path(file)
+# 	abort "ERROR: File not found : #{flac_file}" unless File.exists? flac_file
+# 	abort "ERROR: Not a flac file : #{flac_file}" unless is_flac? flac_file
+# 	comments = IO.popen("metaflac --export-tags-to=- #{Shellwords.escape(flac_file)}").readlines
+# 	ogg_file = flac_file.split(".flac")[0] + ".ogg"
+# 	comments_cmd = comments.collect { |line| "-c \"#{line.split("\n")[0]}\"" }.join(" ")
+# 	#convertIO = IO.popen("flac -d -c #{Shellwords.escape(flac_file)} | oggenc - #{comments_cmd} -o #{Shellwords.escape(ogg_file)}") { |f| f.read }
+# 	#puts "flac -d -c #{Shellwords.escape(flac_file)} | oggenc -q 7 -Q - #{comments_cmd} -o #{Shellwords.escape(ogg_file)}"
+# 	`flac -d -c #{Shellwords.escape(flac_file)} | oggenc -q 7 -Q - #{comments_cmd} -o #{Shellwords.escape(ogg_file)}`
+# end
+
+#############################################################
+# TODO: backup
+#!/bin/bash
+#if test $USER != "root"; then
+#    echo "This tool backups all the system and then needs the root privilege"
+#    exit 1
+#fi
+# RSYNC="--delete --exclude /dev --exclude /sys --exclude /proc --exclude /media --exclude /mnt --exclude /tmp"
+#
+# if test "$1" = "-y"; then
+#     REPLY="y"
+# else
+#     echo "rsync -avn $RSYNC / /media/AGRMOBILE/ > /tmp/backup.log" | sudo sh
+#     less /tmp/backup.log
+#     rm /tmp/backup.log
+#     echo    "---------------------"
+#     read -p "Confirm rsync ? (y/n)"
+# fi
+#
+# if [ "$REPLY" != "y" ]; then
+#     exit
+# fi
+#
+# sudo rsync -av --progress $RSYNC / /media/AGRMOBILE/
+
+#############################################################
+# codelen.py
+#
+# #!/usr/bin/python
+# import sys
+#
+# if len(sys.argv) != 2:
+#   sys.exit("invalid arguments")
+#
+# try:
+#   file = open(sys.argv[1], "rb")
+# except IOError:
+#   sys.exit("could not open %r" % sys.argv[1])
+#
+# longest = ""
+# longest_on = count = 0
+# for line in file:
+#   count += 1
+#   if len(line) > len(longest):
+#     longest = line
+#     longest_on = count
+#
+# longest = longest.rstrip("\n")
+# print "File has total of", count, "lines."
+# print "Longest line found on line", longest_on, "with", len(longest), "characters."
+# print "The line is:", longest
+
+#############################################################
+# TODO: check if still usefull, if true make a rule for it
+#
+# disper
+# valgrind
+# autodownload java/bfg-1.11.7.jar ???
+
+
+#############################################################
+# ipadbook.sh
+# #!/bin/bash
+# curl -i -s -F author="$1" -F datafile=@"$2" http://192.168.0.3:8080/post
+
+#############################################################
+# TODO: ipban, restore, ...
+# #!/bin/bash
+# iptables -A INPUT -s $1 -j DROP
+
+#############################################################
+# Multiple downloads. Should also give possibility to instruct about paging system trough css class
+# #!/usr/bin/ruby
+# require "rubygems"
+# require "open-uri"
+# require "nokogiri"
+# require "escape"
+# require "uri"
+
+# url = ARGV[0]
+# prefix = ARGV[1] || ""
+# doc = Nokogiri::HTML(open(url))
+# uri = URI.parse(url)
+# exclude_ext = "html,htm,php,jpg,jpeg".split(",")
+# already_downloaded = []
+# doc.css("a").each do |link|
+# 	l = link["href"].strip
+# 	if l =~ /\.[a-zA-Z0-9]+$/
+# 		file = l.split("/").last
+# 		ext = file.split(".").last.downcase
+# 		if already_downloaded.count(l).zero? and exclude_ext.count(ext).zero?
+# 			if l.downcase !=~ /^[a-z]+:\/\//
+# 				l = uri.merge(l).to_s
+# 			end
+# 			#w = `#{Escape.shell_command(["wget", "-q", l])}`
+# 			dest = prefix + file
+# 			puts "Downloading #{l} -> #{dest}"
+# 			w = `#{Escape.shell_command(["curl", "-o", dest, l])}`
+# 			already_downloaded << l
+# 		else
+# 			puts "Not downloading #{l}"
+# 		end
+# 	end
+# end
+
+#############################################################
+# OSX reload audio
+# #!/usr/bin/ruby
+# #!/bin/bash
+# sudo kextunload /System/Library/Extensions/AppleHDA.kext
+# sudo kextload /System/Library/Extensions/AppleHDA.kext
+
+#############################################################
+# fun with words and letters
+# #!/usr/bin/python
+# import sys
+# from itertools import permutations
+#
+# if len(sys.argv) != 2:
+# 	print 'Usage: permutations <letters>'
+# 	sys.exit(1)
+#
+# letters = sys.argv[1]
+# for i in permutations(letters, len(letters)):
+# 	print ''.join(i)
+
+#############################################################
+# linux screen shot
+# #!/bin/bash
+# default_app="qiv"
+# APP=${1:-$default_app}
+#
+# scrot "/tmp/%Y-%m-%d_$wx$h.png" -e "$APP \$f &" -s
