0a91d6079298ff351c5b61ea22bdf6a9a1fdb22f
Fabien Meghazi <agr@amigrave.com>
2015-05-13 18:07:14 +0200

    added python shell startup script

---
+++ dotfiles/python/shell_startup.py
@@ -0,0 +1,16 @@
+try:
+    import readline
+    import rlcompleter
+    import atexit
+    import os
+    import sys
+except ImportError:
+    pass
+else:
+    histfile = os.path.join(os.environ["HOME"], ".pythonhistory")
+    readline.parse_and_bind("tab: complete")
+    if os.path.isfile(histfile):
+        readline.read_history_file(histfile)
+    atexit.register(readline.write_history_file, histfile)
+    sys.ps1 = '\033[01;33m>>>\033[00m '
+    del os, histfile, readline, rlcompleter, atexit, sys
