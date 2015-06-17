507b42cd165f055f3eb54d7073354e0bade6c736
Fabien Meghazi <agr@amigrave.com>
2015-06-17 08:17:20 +0200

    add todo in start

--- start.sh
+++ start.sh
@@ -22,7 +22,20 @@ fi
 # ln -s dotfiles/bash/bashrc $HOME/.bashrc
 
 # Should be portable as much as possible, so get rid of this
+# ln -s dotfiles/mc $HOME/.config/mc
+# ln -s dotfiles/pudb $HOME/.config/pudb
+# ln -s dotfiles/mc/skins  $HOME/.local/share/mc/skins/
 # ln -s dotfiles/terminator $HOME/.config/terminator
 # ln -s dotfiles/bazaar $HOME/.config/bazaar
 # ln -s dotfiles/git $HOME/.config/git
 # ln -s dotfiles/kid3.sourceforge.net $HOME/.config/kid3.sourceforge.net
+
+
+# Debian QTile
+# git clone https://github.com/qtile/qtile.git ~/.local/qtile
+# sudo apt-get install python-dev libffi-dev python-pip python-trollius python-cairocffi libpangocairo-1.0-0 sudo apt-get install libxcb-render0-dev
+# pip install --user xcffib
+
+
+# Odoo
+# sudo apt-get install postgresql
